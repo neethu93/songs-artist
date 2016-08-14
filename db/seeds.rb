c176f6649be87c9f15f847185fde2b3a98b307a7
neethu93 <pingneethu@gmail.com>
2016-08-15 00:45:46 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Song.create( { title: "Poker Face", language: "English", image_url: "http://images3.wikia.nocookie.net/__cb20091119022759/ladygaga/images/5/5c/PokerFace(DigitalCover).png" } )
+Song.create( { title: "Mounam", language: "Malayalam", image_url: "https://i.ytimg.com/vi/Knc9Hf34wDM/maxresdefault.jpg" } )
+Song.create( { title: "Chupke Chupke", language: "Hindi", image_url: "https://3.bp.blogspot.com/-QIUTG8IyJiI/VufLcM24YdI/AAAAAAAAHBY/qMMTUO2EMFsrHU7T2UrvrWXj22TZfDCCg/s400/chori-chori-chupke-chupke.jpg" } )
+
+
+Artist.create( { name: "lady gaga",image_url: "https://media1.popsugar-assets.com/files/2016/01/10/138/n/1922398/49aaa839caa8207b_GettyImages-504396978.xxxlarge_2x.jpg" } )
+Artist.create( { name: "Stephen devassy", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Stephen_BNC.jpg/220px-Stephen_BNC.jpg" } )
+Artist.create( { name: "Anu malik",image_url: "http://content.bollywoodhungama.com/movie/display%20image/300x275%20jpeg/52364745.jpg" } )
+
+
